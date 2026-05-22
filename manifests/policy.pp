790668f4d150d0385e6b4dafa7be436bee957487
Ulrich Schwickerath <ulrich.schwickerath@cern.ch>
2026-05-22 12:49:30 +0200

    add option which allows to manage policies outside this module
    Change-Id: I9a1b5a987e3ca82c01f5e311376bacdb18d74766

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -23,9 +23,14 @@
 #   (optional) Path to the nova policy.json file
 #   Defaults to /etc/blazar/policy.yaml
 #
+# [*create_resources*]
+#   (optional) use this module to manage the policies
+#    Defaults to true
+#
 class blazar::policy (
-  Hash   $policies    = {},
-  String $policy_path = '/etc/blazar/policy.yaml',
+  Hash    $policies         = {},
+  String  $policy_path      = '/etc/blazar/policy.yaml',
+  Boolean $create_resources = true,
 ) {
 
   include blazar::deps
@@ -35,8 +40,9 @@ class blazar::policy (
     file_format => 'yaml',
   }
 
-  create_resources('openstacklib::policy::base', $policies)
+  if $create_resources {
+    create_resources('openstacklib::policy::base', $policies)
 
-  oslo::policy { 'blazar_config': policy_file => $policy_path }
-
+    oslo::policy { 'blazar_config': policy_file => $policy_path }
+  }
 }
